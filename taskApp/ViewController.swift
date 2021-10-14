35a5fefa99aa84f3a9c2cbecee439b2685551fff
Olimdzhon <write2sd@mail.ru>
2021-10-14 11:22:50 +0500

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by Олимджон Садыков on 14.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
