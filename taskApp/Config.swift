648705052d99f3ec3cadd4ce825b6183b1251cf2
Olimdzhon <write2sd@mail.ru>
2021-10-22 09:55:52 +0500

    url fix

--- taskApp/Config.swift
+++ taskApp/Config.swift
@@ -10,10 +10,10 @@ class Config {
     static let shared = Config()
     private init(){}
 
-    static let SERVER_ADDRESS = "localhost"
-    static let SERVER_PORT = ":3000"
-    static let SERVER_URL = "http://\(SERVER_ADDRESS)\(SERVER_PORT)"
-    static let SERVER_IMAGES = "http://\(SERVER_ADDRESS)\(SERVER_PORT)/assets/images/"
+    static let SERVER_ADDRESS = "meg-backend.herokuapp.com"
+    static let SERVER_PORT = ""
+    static let SERVER_URL = "https://\(SERVER_ADDRESS)\(SERVER_PORT)"
+    static let SERVER_IMAGES = "https://\(SERVER_ADDRESS)\(SERVER_PORT)/assets/images/"
 }
 
 private class DevConfig {
